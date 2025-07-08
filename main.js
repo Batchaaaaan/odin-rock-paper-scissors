365f98020c20c95f94586f0e2aa7d71b09f84ee5
Batchaaaaan <ctian.arreola22@gmail.com>
2025-07-08 21:43:11 +0800

    Add UI into the game
    Added a bunch of functions

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+window.onload = function () {
+    playGame();
+    }
 
 
 function getComputerChoice() {
@@ -12,56 +15,119 @@ function getComputerChoice() {
 }
 
 
-function getHumanChoice() {
-    let choice = prompt("Choose 1: Rock, Paper or Scissors? :")
-    return choice
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return 'Draw!';
+    } else if ((humanChoice === 'Rock' &&  computerChoice === 'Scissors')
+            || (humanChoice === 'Paper' &&  computerChoice === 'Rock')
+            || (humanChoice === 'Scissors' &&  computerChoice === 'Paper')) {
+        return `You won a round! ${humanChoice} beats ${computerChoice}`
+    } else {
+        return `You lost a round! ${computerChoice} beats ${humanChoice}`;
+    }
 }
 
 
-function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase()
-        computerChoice = computerChoice.toLowerCase()
+function getButtonID(event) {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton) {
+        return;
+    }
+    return event.target.id;
+}
 
-        if (humanChoice === computerChoice) {
-            return 'Draw!'
-        } else if (humanChoice === 'rock' &&  computerChoice === 'scissors') {
-            return 'You win! Rock beats Scissors'
-        } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-            return 'You win! Paper beats Rock'
-        } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-            return 'You win! Scissors beat Paper'
-        } else {
-            return 'You Lose'
-        }
+
+function updateRound(roundNumber) {
+    const round = document.querySelector('#round-section');
+    round.textContent = `Round ${roundNumber}`
+    return;
+}
+
+
+function updateHumanScore(humanScore) {
+    const score = document.querySelector('#human-score');
+    score.textContent = (`Human's Score: ${humanScore}`);
+    return;
+}
+
+
+function updateComputerScore(computerScore) {
+    const score = document.querySelector('#computer-score');
+    score.textContent = (`Computer's Score: ${computerScore}`);
+    return;
+}
+
+
+function updateResults (roundResult) {
+    const result = document.querySelector('#results');
+    result.textContent = roundResult;
+    return;
+}
+
+
+function scoreChecker(humanScore, computerScore) {
+    const maxScore = 5;
+
+    if (humanScore >= maxScore) {
+        const h_score = document.querySelector('#results');
+        h_score.textContent = `You got ${humanScore} Points! Well Played.`;
+        return true
+    } else if (computerScore >= maxScore) {
+        const c_score = document.querySelector('#results');
+        c_score.textContent = `Computer got ${computerScore} Points. Better luck next time.`;
+        return true
+    } else {
+        return false;
+    }
 }
 
 
 function playGame () {
     let humanScore = 0;
     let computerScore = 0;
+    let roundNumber = 1;
+    let scoreOver5 = false;
 
-    for (let i=1; i<=5; i++) {
-        console.log(`Round ${i} of 5`)
-        const humanSelection = getHumanChoice()
-        const computerSelecion = getComputerChoice()
+    const wrapper = document.querySelector('#buttons');
 
-        console.log(`Human's Score: ${humanScore}`, `Computer's Score: ${computerScore}`)
-        roundResult = playRound(humanSelection, computerSelecion)
-        if (roundResult.startsWith('You win')) {
-            humanScore++
-        } else if (roundResult.startsWith('You lose')) {
-            computerScore++
+    roundResult = wrapper.addEventListener('click', function(event) {
+        const humanSelection = getButtonID(event);
+        const computerSelection = getComputerChoice();
+        
+        if (scoreOver5 === true) {
+            humanScore = 0;
+            computerScore = 0;
+            roundNumber = 1;
+            updateComputerScore(computerScore);
+            updateHumanScore(humanScore);
+            updateRound(roundNumber);
+            updateResults('Restarted Game')
+            scoreOver5 = false;
+            
+        } else {
+            const result = playRound(humanSelection, computerSelection);
+            scoreChecker(humanScore, computerScore)
+            console.log(result);
+            
+            if (result === '' || result === undefined) {
+                return ''
+            } else if (result.includes('won')) {
+                humanScore++;
+                updateHumanScore(humanScore);
+                updateRound(roundNumber++);
+                updateResults(result);
+                scoreOver5 = scoreChecker(humanScore, computerScore)
+                return result;
+            } else if (result.includes('lost')) {
+                computerScore++;
+                updateComputerScore(computerScore);
+                updateRound(roundNumber++);
+                updateResults(result);
+                scoreOver5 = scoreChecker(humanScore, computerScore)
+                return result;
+            } else {
+                updateResults(result);
+            }
         }
-        
-        console.log(roundResult)
-    }
-
-    if (humanScore > computerScore) {
-        return `You won! You scored ${humanScore} points against ${computerScore} computer's score`
-    } else if (humanScore < computerScore) {
-        return `You lost! You scored ${humanScore} points against ${computerScore} computer's score`
-    } else {
-        return 'It was a draw!'
-    }
+    })
 }
-        
